5273340f176e85851834035994b3f3b6db5542a9
Rob Nugen <rob@robnugen.com>
2015-05-13 23:48:58 +0900

    use enclosure to rebuild pushups.js
    Thanks https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures

--- js/pushups.js
+++ js/pushups.js
@@ -1,34 +1,33 @@
-var nosedownCounter = {
+var makeNosedownCounter = function() {
   var myCookieGUID;
   var cookieName = 'push-up-cookie';
-  var recentNosedowns;
+  var privateNosedownCounter = 0;
 
-  initialize: function(app_id,js_key) {
-    Parse.initialize(config.app_id, config.js_key);
-    if(docCookies.hasItem(cookieName)) {
-      myCookieGUID = docCookies.getItem(cookieName);
-    } else {
-      myCookieGUID = guid();
-      docCookies.setItem(cookieName,myCookieGUID);
-    }
-  },
-  doNoseDown: function() {
-    recentNosedowns++;
-    return recentNosedowns;
-  },
-  getNoseDowns: function() {
-    return recentNosedowns;
-  },
-  saveNoseDowns: function() {
-    var PushupsPerformedBy = Parse.Object.extend("PushupsPerformedBy");
-    var myPushupCounts = new PushupsPerformedBy();
-    myPushupCounts.save({guid: myCookieGUID, pushups:recentNosedowns}, {
-      success: function(object) {
-        recentNosedowns = 0;
-      },
-      error: function(model, error) {
-        $(".error").show();
-      }
-    });
+  Parse.initialize(config.app_id, config.js_key);
+  if(docCookies.hasItem(cookieName)) {
+    myCookieGUID = docCookies.getItem(cookieName);
+  } else {
+    myCookieGUID = guid();
+    docCookies.setItem(cookieName,myCookieGUID);
   }
-}
+  return {
+    doNosedown: function() {
+      privateNosedownCounter++;
+    },
+    saveNosedowns: function() {
+      var PushupsPerformedBy = Parse.Object.extend("PushupsPerformedBy");
+      var myPushupCounts = new PushupsPerformedBy();
+      myPushupCounts.save({guid: myCookieGUID, pushups:privateNosedownCounter}, {
+        success: function(object) {
+          privateNosedownCounter = 0;
+        },
+        error: function(model, error) {
+          $(".error").show();
+        }
+      });
+    },
+    getNosedowns: function() {
+      return privateNosedownCounter;
+    }
+  }  
+};
