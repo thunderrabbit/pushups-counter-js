5e5e198800468658914823c0ea290329e3998e64
Rob Nugen <rob@robnugen.com>
2015-05-15 00:47:22 +0900

    Comment out TodoList

--- js/nosedown_counter.js
+++ js/nosedown_counter.js
@@ -44,34 +44,34 @@ $(function() {
   // Todo Collection
   // ---------------
 
-  var TodoList = Parse.Collection.extend({
+  // var TodoList = Parse.Collection.extend({
 
-    // Reference to this collection's model.
-    model: Todo,
+  //   // Reference to this collection's model.
+  //   model: Todo,
 
-    // Filter down the list of all todo items that are finished.
-    done: function() {
-      return this.filter(function(todo){ return todo.get('done'); });
-    },
+  //   // Filter down the list of all todo items that are finished.
+  //   done: function() {
+  //     return this.filter(function(todo){ return todo.get('done'); });
+  //   },
 
-    // Filter down the list to only todo items that are still not finished.
-    remaining: function() {
-      return this.without.apply(this, this.done());
-    },
+  //   // Filter down the list to only todo items that are still not finished.
+  //   remaining: function() {
+  //     return this.without.apply(this, this.done());
+  //   },
 
-    // We keep the Todos in sequential order, despite being saved by unordered
-    // GUID in the database. This generates the next order number for new items.
-    nextOrder: function() {
-      if (!this.length) return 1;
-      return this.last().get('order') + 1;
-    },
+  //   // We keep the Todos in sequential order, despite being saved by unordered
+  //   // GUID in the database. This generates the next order number for new items.
+  //   nextOrder: function() {
+  //     if (!this.length) return 1;
+  //     return this.last().get('order') + 1;
+  //   },
 
-    // Todos are sorted by their original insertion order.
-    comparator: function(todo) {
-      return todo.get('order');
-    }
+  //   // Todos are sorted by their original insertion order.
+  //   comparator: function(todo) {
+  //     return todo.get('order');
+  //   }
 
-  });
+  // });
 
   // Todo Item View
   // --------------
